b8e38845e969d967a617c4fea4390e1392df9f99
turehazi <truehazi@gmail.com>
2013-10-21 09:43:59 +0800

    modify install cmd

--- spf13-vim-windows-install.cmd
+++ spf13-vim-windows-install.cmd
@@ -3,7 +3,7 @@
 
 @set BASE_DIR=%HOME%\.spf13-vim-3
 IF NOT EXIST "%BASE_DIR%" (
-  call git clone --recursive -b 3.0 https://github.com/spf13/spf13-vim.git "%BASE_DIR%"
+  call git clone --recursive -b 3.0 https://github.com/FlyingHazi/spf13-vim.git "%BASE_DIR%"
 ) ELSE (
 	@set ORIGINAL_DIR=%CD%
     echo updating spf13-vim
@@ -15,8 +15,10 @@ IF NOT EXIST "%BASE_DIR%" (
 
 call mklink "%HOME%\.vimrc" "%BASE_DIR%\.vimrc"
 call mklink "%HOME%\_vimrc" "%BASE_DIR%\.vimrc"
+call mklink "%HOME%\_vimrc.local" "%BASE_DIR%\.vimrc.local"
 call mklink "%HOME%\.vimrc.fork" "%BASE_DIR%\.vimrc.fork"
 call mklink "%HOME%\.vimrc.bundles" "%BASE_DIR%\.vimrc.bundles"
+call mklink "%HOME%\.vimrc.bundles.local" "%BASE_DIR%\.vimrc.bundles.local"
 call mklink "%HOME%\.vimrc.bundles.fork" "%BASE_DIR%\.vimrc.bundles.fork"
 call mklink "%HOME%\.vimrc.before" "%BASE_DIR%\.vimrc.before"
 call mklink "%HOME%\.vimrc.before.fork" "%BASE_DIR%\.vimrc.before.fork"
